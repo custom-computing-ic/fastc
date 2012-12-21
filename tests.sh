8b6813b08a4b7e35a43928b70a90ca10e6b5fc4b
Paul Grigoras <paul.grigoras90@gmail.com>
2012-12-21 12:04:01 +0000

    Add a simple test file II.

---
+++ tests.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+############ Configuration ##############
+
+EXE=maxcc
+
+
+############ Test Functions #############
+
+runTest() {
+    printf "Running $1..."
+    ${EXE} test/$1.c > test/$1.out
+    output=`diff test/$1.out test/$1.exp`
+    res=$?
+    if [ "$res" = "1" ]; then
+	printf "[Failed!]\n"
+	printf " ---- Test output ---- \n"
+	printf "$output\n"
+	printf " ---------------------\n"
+    else 
+	printf "[Passed!]\n"
+    fi
+}
+
+
+############# Test Setup #################
+
+runTest "testMaxCTemplate"
+
+############# Test Summary ###############
