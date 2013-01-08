2a6af68d9e7786eb7c2ca28679bd78ed9bc58bdb
Paul Grigoras <paul.grigoras90@gmail.com>
2013-01-08 16:40:48 +0000

    Add CmdWrite test.

--- test/maxc/testCmdWrite.c
+++ test/maxc/testCmdWrite.c
@@ -1,25 +1,30 @@
 #define burst_inc 1
 
 #pragma scalar in iniBursts uint32 kernel_Cmdwrite
+#pragma scalar in iterations uint32 kernel_Cmdwrite
 #pragma scalar in totalBursts uint32 kernel_Cmdwrite
 #pragma scalar in wordsPerBurst uint32 kernel_Cmdwrite
-#pragma scalar in Enable uint32 kernel_Cmdwrite
-void kernel_Cmdwrite(
-                     int iniBursts, int totalBursts, int wordsPerBurst,
-                     int Enable) {
+#pragma scalar in Enable uint1 kernel_Cmdwrite
+void kernel_Cmdwrite(int iniBursts,
+                         int iterations,
+                         int totalBursts,
+                         int wordsPerBurst,
+                         int Enable) {
 
-    int wordCount  = count_p(32, wordsPerBurst, 1, Enable);
+int wordCount  = count_p(32, wordsPerBurst, 1, Enable);
 
-    int wrap = (wordCount == wordsPerBurst);
-    int burstCount = count_p(32, burst_inc, 1, wrap);
+int wrap = (wordCount == wordsPerBurst);
+int burstCount = count_p(32, totalBursts, 1, wrap);
 
-    int Control = (wordCount == 0) & Enable;
+int wrap2 = (burstCount == totalBursts);
+int iterCount = count_p(32, iterations, 1, wrap2);
 
-    DRAMOutput("dram_read",
+int Control = (wordCount == 0) & Enable;
+DRAMOutput("dram_write",
                Control,
                burstCount + iniBursts,
                burst_inc,
                1,
                0,
-               0);
+               (burstCount == totalBursts - burst_inc) && iterCount == iterations - 1);
 }
