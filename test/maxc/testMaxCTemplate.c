16889f88043253425050eca8b02ef2c00f8489ac
Paul Grigoras <paul.grigoras90@gmail.com>
2013-01-08 08:41:40 +0000

    Fix test.

--- test/maxc/testMaxCTemplate.c
+++ test/maxc/testMaxCTemplate.c
@@ -1,15 +1,13 @@
-typedef int stream_t;
-typedef int scalar_array;
 typedef int count_t;
 
-//void input(stream_t *x, int dim) {}
-void output(stream_t* out, int res, int cond) {}
+extern void output_ic(int* out, int res, int cond);
+extern void output_i(int* out, int res);
+
 void chain(count_t i, count_t j, count_t k) {}
 int fselect(int cond, int val1, int val2) {return (cond ==0? val1 : val2);}
 count_t count(int wrap, int inc) {return 1;}
 count_t countChain(int wrap, int inc, count_t parent) {return 1;}
 int scalar(){return 1;}
-//void input_scalar_array(scalar_array *c, int count) {}
 
 #pragma in x CPU kernel_KernelFunc
 #pragma out res CPU kernel_KernelFunc
@@ -23,13 +21,13 @@ int scalar(){return 1;}
 #pragma scalar in nx uint32 kernel_KernelFunc
 #pragma scalar in ny uint32 kernel_KernelFunc
 #pragma scalar in nz uint32 kernel_KernelFunc
-void kernel_KernelFunc(stream_t *x, stream_t *res,
-		 int nx, int ny, int nz, 
+void kernel_KernelFunc(int *x, int *res,
+		 int nx, int ny, int nz,
 		 int a1, int a2, int a3,
-		 int n1, int n2, int n3, 
+		 int n1, int n2, int n3,
 		 int c) {
 
-  // counters are required to keep track of point being 
+  // counters are required to keep track of point being
   // processed and determine if this is within boundaries
   count_t i = count(nz, 1);
   count_t j = countChain(ny, 1, i);
@@ -39,9 +37,9 @@ void kernel_KernelFunc(stream_t *x, stream_t *res,
   int cond = (i >= a1 & i < n1) & (j >= a2 & j < n2) & (k >= a3 & k < n3);
 
   int func =
-          c * x[0] + 
+          c * x[0] +
           c * x[1]      + c * x[-1] +
-          c * x[-nx]    + c * x[nx] + 
+          c * x[-nx]    + c * x[nx] +
           c * x[-nx*ny] + c * x[nx*ny];
 
 
@@ -49,5 +47,5 @@ void kernel_KernelFunc(stream_t *x, stream_t *res,
   // otherwise assign a default value (0)
   // int r = fselect(cond, func, 0);
 
-  output(res, func, cond);
+  output_ic(res, func, cond);
 }
