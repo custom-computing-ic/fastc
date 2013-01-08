dad628791cd927c16b90822c35491253a5ba1171
Paul Grigoras <paul.grigoras90@gmail.com>
2013-01-08 22:27:18 +0000

    Add test files for Convolution Kernel.

---
+++ test/maxc/test1dConvolutionKernel.c
@@ -0,0 +1,26 @@
+#pragma in p CPU kernel_Convolution1d
+#pragma scalar in c_0_0_0 float8_24 kernel_Convolution1d
+#pragma scalar in c_p_0_0 float8_24 kernel_Convolution1d
+#pragma scalar in c_n_0_0 float8_24 kernel_Convolution1d
+void kernel_Convolution1d(
+                          float *p,
+                          float *output,
+                          float c_0_0_0,
+                          float c_p_0_0,
+                          float c_n_0_0,
+                          int n1,
+                          int ORDER) {
+    int i4 = count(1000, 1);
+    int i1 = countChain(n1, 1, i4);
+
+    int up = (i1 >= ORDER) && (i1 < n1 - ORDER);
+
+    int result =
+        p[-1] * c_n_0_0 +
+        p[0] * c_0_0_0 +
+        p[1] * c_p_0_0;
+
+    int inter = fselect(up, result, p);
+
+    output_i(output, inter);
+}
