a2148bd438e798cd51681a0d8637dc05128d82d0
Paul Grigoras <paul.grigoras90@gmail.com>
2013-02-04 02:48:55 +0000

    Update counter test. Remove projects/ dir.

--- tests.sh
+++ tests.sh
@@ -48,7 +48,7 @@ runLocalTestSuite() {
 
         runTest "testPassThrough/maxc/testPassThroughKernel"
 
-        #    runTest "testTypes/testCounter"
+        runTest "testCounter/maxc/testCounter"
 
         #    runTest "maxc/testRTMManager"
 
