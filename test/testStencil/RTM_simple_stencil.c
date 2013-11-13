c0605824d2e536200a60614ad7c2e1b36a6aea64
Paul Grigoras <paul.grigoras90@gmail.com>
2013-11-13 18:16:23 +0000

    Update the stencil test.
    Former-commit-id: d74a10ecfc3a4248223a3ad1d08dda28ae47cbca

--- test/testStencil/RTM_simple_stencil.c
+++ test/testStencil/RTM_simple_stencil.c
@@ -12,56 +12,43 @@ void kernel_f3(int * a, int *b, int *c) {
 
 #define N1 128
 #define N12 16384
-void kernel_do_step(float* dvv, float * p, float *pp, float *pp_out, float *conv, float *source,
-		    int n1, int n2, int n3, int n12, int ORDER) {
-
-    float c_0   = -0.000000056202665632554272;
-    float c_1_0 =  0.000000010666666661052204;
-    float c_1_1 = -0.000000001523809300962853;
-    float c_1_2 =  0.000000000253968179819708;
-    float c_1_3 = -0.000000000031745984313547;
-    float c_1_4 =  0.000000000002031744130576;
-    float c_2_0 =  0.000000010666666661052204;
-    float c_2_1 = -0.000000001523809300962853;
-    float c_2_2 =  0.000000000253968179819708;
-    float c_2_3 = -0.000000000031745984313547;
-    float c_2_4 =  0.000000000002031744130576;
-    float c_3_0 =  0.000000010666666661052204;
-    float c_3_1 = -0.000000001523809300962853;
-    float c_3_2 =  0.000000000253968179819708;
-    float c_3_3 = -0.000000000031745984313547;
-    float c_3_4 =  0.000000000002031744130576;
-    
+void kernel_do_step(float* dvv, float * p, float *pp, float *pp_out, float *conv,
+                    float *source,
+                    int n1, int n2, int n3, int n12, int ORDER,
+                    float c_0, float c_1_0, float c_1_1, float c_1_2, float c_1_3,
+                    float c_1_4, float c_2_0, float c_2_1, float c_2_2, float c_2_3,
+                    float c_2_4, float c_3_0, float c_3_1, float c_3_2,
+                    float c_3_3, float c_3_4) {
 
 #pragma fast stencil
-  for(int i3=ORDER; i3 < n3-ORDER; i3++){  
+  for(int i3=ORDER; i3 < n3-ORDER; i3++){
     for(int i2=ORDER; i2 < n2-ORDER; i2++){
       for(int i1=ORDER; i1< n1-ORDER; i1++){
 
-	conv[i1+i2*N1+i3*N12] =
-	  dvv[i1+i2*N1+i3*N12]
-	  *(
-	    p[i1+i2*N1+i3*N12]*c_0
-	    +c_1_0*(p[(i1+1)+(i2  )*N1+(i3  )*N12]+p[(i1-1)+(i2  )*N1+(i3  )*N12])
-	    +c_1_1*(p[(i1+2)+(i2  )*N1+(i3  )*N12]+p[(i1-2)+(i2  )*N1+(i3  )*N12])
-	    +c_1_2*(p[(i1+3)+(i2  )*N1+(i3  )*N12]+p[(i1-3)+(i2  )*N1+(i3  )*N12])
-	    +c_1_3*(p[(i1+4)+(i2  )*N1+(i3  )*N12]+p[(i1-4)+(i2  )*N1+(i3  )*N12])
-	    +c_1_4*(p[(i1+5)+(i2  )*N1+(i3  )*N12]+p[(i1-5)+(i2  )*N1+(i3  )*N12])
-	    +c_2_0*(p[(i1  )+(i2+1)*N1+(i3  )*N12]+p[(i1  )+(i2-1)*N1+(i3  )*N12])
-	    +c_2_1*(p[(i1  )+(i2+2)*N1+(i3  )*N12]+p[(i1  )+(i2-2)*N1+(i3  )*N12])
-	    +c_2_2*(p[(i1  )+(i2+3)*N1+(i3  )*N12]+p[(i1  )+(i2-3)*N1+(i3  )*N12])
-	    +c_2_3*(p[(i1  )+(i2+4)*N1+(i3  )*N12]+p[(i1  )+(i2-4)*N1+(i3  )*N12])
-	    +c_2_4*(p[(i1  )+(i2+5)*N1+(i3  )*N12]+p[(i1  )+(i2-5)*N1+(i3  )*N12])
-	    +c_3_0*(p[(i1  )+(i2  )*N1+(i3+1)*N12]+p[(i1  )+(i2  )*N1+(i3-1)*N12])
-	    +c_3_1*(p[(i1  )+(i2  )*N1+(i3+2)*N12]+p[(i1  )+(i2  )*N1+(i3-2)*N12])
-	    +c_3_2*(p[(i1  )+(i2  )*N1+(i3+3)*N12]+p[(i1  )+(i2  )*N1+(i3-3)*N12])
-	    +c_3_3*(p[(i1  )+(i2  )*N1+(i3+4)*N12]+p[(i1  )+(i2  )*N1+(i3-4)*N12])
-	    +c_3_4*(p[(i1  )+(i2  )*N1+(i3+5)*N12]+p[(i1  )+(i2  )*N1+(i3-5)*N12]));
+        conv[i1+i2*N1+i3*N12] =
+          dvv[i1+i2*N1+i3*N12]
+          *(
+            p[i1+i2*N1+i3*N12]*c_0
+            +c_1_0*(p[(i1+1)+(i2  )*N1+(i3  )*N12]+p[(i1-1)+(i2  )*N1+(i3  )*N12])
+            +c_1_1*(p[(i1+2)+(i2  )*N1+(i3  )*N12]+p[(i1-2)+(i2  )*N1+(i3  )*N12])
+            +c_1_2*(p[(i1+3)+(i2  )*N1+(i3  )*N12]+p[(i1-3)+(i2  )*N1+(i3  )*N12])
+            +c_1_3*(p[(i1+4)+(i2  )*N1+(i3  )*N12]+p[(i1-4)+(i2  )*N1+(i3  )*N12])
+            +c_1_4*(p[(i1+5)+(i2  )*N1+(i3  )*N12]+p[(i1-5)+(i2  )*N1+(i3  )*N12])
+            +c_2_0*(p[(i1  )+(i2+1)*N1+(i3  )*N12]+p[(i1  )+(i2-1)*N1+(i3  )*N12])
+            +c_2_1*(p[(i1  )+(i2+2)*N1+(i3  )*N12]+p[(i1  )+(i2-2)*N1+(i3  )*N12])
+            +c_2_2*(p[(i1  )+(i2+3)*N1+(i3  )*N12]+p[(i1  )+(i2-3)*N1+(i3  )*N12])
+            +c_2_3*(p[(i1  )+(i2+4)*N1+(i3  )*N12]+p[(i1  )+(i2-4)*N1+(i3  )*N12])
+            +c_2_4*(p[(i1  )+(i2+5)*N1+(i3  )*N12]+p[(i1  )+(i2-5)*N1+(i3  )*N12])
+            +c_3_0*(p[(i1  )+(i2  )*N1+(i3+1)*N12]+p[(i1  )+(i2  )*N1+(i3-1)*N12])
+            +c_3_1*(p[(i1  )+(i2  )*N1+(i3+2)*N12]+p[(i1  )+(i2  )*N1+(i3-2)*N12])
+            +c_3_2*(p[(i1  )+(i2  )*N1+(i3+3)*N12]+p[(i1  )+(i2  )*N1+(i3-3)*N12])
+            +c_3_3*(p[(i1  )+(i2  )*N1+(i3+4)*N12]+p[(i1  )+(i2  )*N1+(i3-4)*N12])
+            +c_3_4*(p[(i1  )+(i2  )*N1+(i3+5)*N12]+p[(i1  )+(i2  )*N1+(i3-5)*N12]));
 
-        pp_out[i1+i2*N1+i3*N12] = 
-	  2.0 * p[i1+i2*N1+i3*N12]-pp[i1+i2*N1+i3*N12] + 
-	  conv[i1+i2*N1+i3*N12] + source[i1+i2*N1+i3*N12];  
-	
+        pp_out[i1+i2*N1+i3*N12] =
+          2.0 * p[i1+i2*N1+i3*N12]-pp[i1+i2*N1+i3*N12] +
+          conv[i1+i2*N1+i3*N12] + source[i1+i2*N1+i3*N12];
+
       }
     }
   }
@@ -77,13 +64,13 @@ int main() {
   int n12 = n1 * n2;
   int n123 = n1 * n2 * n3;
 
-  
+
   int size = 384;
   int *in = (int *)malloc(sizeof(int) * size);
 
   for (int i = 0; i < 10; i++)
     in[i] = 0;
-  
+
   int *out1 = (int *)malloc(sizeof(int) * size);
   int *out2 = (int *)malloc(sizeof(int) * size);
   int *out3 = (int *)malloc(sizeof(int) * size);
@@ -100,7 +87,7 @@ int main() {
   kernel_f3(out1, out3, out4);
 
   float *dvv = (float *)malloc(sizeof(float) * n123);
-    
+
 #pragma fast hw:kernel_f2
   kernel_f2(out3, dvv);
 
@@ -116,16 +103,33 @@ int main() {
   float *pp = (float *)malloc(sizeof(float) * n123);
   float *pp_out = (float *)malloc(sizeof(float) * n123);
   float *source_container = (float *)malloc(sizeof(float) * n123);
-
-  float c_0 = 0;
-
   float *conv =(float*)malloc(sizeof(float)*n123);
   float *cal =(float*)malloc(sizeof(float)*n123);
 
+  float c_0   = -0.000000056202665632554272;
+  float c_1_0 =  0.000000010666666661052204;
+  float c_1_1 = -0.000000001523809300962853;
+  float c_1_2 =  0.000000000253968179819708;
+  float c_1_3 = -0.000000000031745984313547;
+  float c_1_4 =  0.000000000002031744130576;
+  float c_2_0 =  0.000000010666666661052204;
+  float c_2_1 = -0.000000001523809300962853;
+  float c_2_2 =  0.000000000253968179819708;
+  float c_2_3 = -0.000000000031745984313547;
+  float c_2_4 =  0.000000000002031744130576;
+  float c_3_0 =  0.000000010666666661052204;
+  float c_3_1 = -0.000000001523809300962853;
+  float c_3_2 =  0.000000000253968179819708;
+  float c_3_3 = -0.000000000031745984313547;
+  float c_3_4 =  0.000000000002031744130576;
+
 #pragma fast hw_kernel:kernel_do_step
   kernel_do_step(dvv, p, pp, pp_out, conv, source_container,
-		 n1, n2, n3, n12, ORDER);
-  
+                 n1, n2, n3, n12, ORDER,
+                 c_0,  c_1_0,  c_1_1,  c_1_2,  c_1_3,
+                 c_1_4,  c_2_0,  c_2_1,  c_2_2,  c_2_3,
+                 c_2_4,  c_3_0,  c_3_1,  c_3_2,
+                 c_3_3,  c_3_4);
   return 0;
 
 }
