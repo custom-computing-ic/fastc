5a4301011c2676b27c5e6c65d28418ccd92953a9
Paul Grigoras <paul.grigoras90@gmail.com>
2013-11-19 01:56:10 +0000

    Analyze all pragams preceeding a kernel without requiring func: attribute.
    Former-commit-id: efeee76599ec5f51015eb008683aad9aa467a768

--- src/passes/PragmaExtraction.hxx
+++ src/passes/PragmaExtraction.hxx
@@ -15,23 +15,31 @@ public:
       string *max = get_pragma_param(pragma, "max");
       string *min = get_pragma_param(pragma, "min");
       kernel->addOffsetExpression(*var, *max, *min);
-    } if (*get_pragma_param(pragma, "ioType") != "") {
+    } 
+
+    if (*get_pragma_param(pragma, "ioType") != "") {
       string *ioType = get_pragma_param(pragma, "ioType");
       string *computeType = get_pragma_param(pragma, "computeType");
+      cout << "\t\tvar: '" << *var;
+      if (ioType != NULL)
+        cout << "' ioType: '" << *ioType;
+      if (computeType != NULL)
+        cout << "' computeType: " << *computeType << "'";
+      cout << endl;
       kernel->updateTypeInfo(*var, *ioType, *computeType);
     }
   }
 
   void runPass(Design* design) {
-    Rose_STL_Container<SgNode*> pragmas;
-    pragmas = NodeQuery:: querySubTree(design->getProject(), V_SgPragma);
-    foreach_(SgNode* pragma, pragmas) {
-      SgPragma* p = isSgPragma(pragma);
-      if ( p != NULL ) {
-        string *k = get_pragma_param(p->get_pragma(), "func");
-        Kernel *kernel = design->getKernel(*k);
-        if (kernel != NULL)
-          updateKernelDeclaration(kernel, p->get_pragma());
+    foreach_(Kernel* k, design->getKernels()) {
+      SgFunctionDeclaration* fdecl = k->getDeclaration();
+      vector<SgPragma*> pragmas;
+      cout << "\tAnalysing annotated type info for kernel " << k->getName() << endl;
+      SgStatement* stmt = SageInterface::getPreviousStatement(fdecl);
+      SgPragmaDeclaration* p;
+      while ((p = isSgPragmaDeclaration(stmt)) != NULL) {
+        updateKernelDeclaration(k, p->get_pragma()->get_pragma());
+        stmt = SageInterface::getPreviousStatement(stmt);
       }
     }
   }
