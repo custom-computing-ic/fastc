806947928bcc4ca6a7cdec4709a91152132fd8bf
Paul Grigoras <paul.grigoras90@gmail.com>
2014-05-06 15:42:04 +0100

    Move run to setup.sh
    Former-commit-id: 6f31e57f1483db3bbad92e24312bd545b0c4bc43

--- demos/rtm-adp/setup.sh
+++ demos/rtm-adp/setup.sh
@@ -45,11 +45,51 @@ function help {
     echo "  -h   print the help message"
 }
 
+function run {
+    source settings.sh
+
+    sources=`find src -name "*.c" | tr "\\n" " "`
+
+    echo "1. Loading ADP - RTM demo"
+    echo "  harmonic ==> "`which harmonic`
+    echo "  fastc   ==> "`which maxcc`
+    echo "  sources  ==> $sources"
+    echo "  aspects  ==> $ASP_REP"
+
+    rm -rf designs-RTM
+    rm -rf designs-ADP
+
+    #--- RTM Code generation
+    # cp src/RTMKernel.c .
+    # echo "[RTM] 1. Running Harmonic Weaver"
+    # larai $ASP_REP/main.lara -d -a "{csources:'RTMKernel.c', cflags: '-Iinclude/', cparams:[ { Par: 1, Mul: 1}, { Par: 2, Mul: 1 } ], mants:[22, 24]}"
+    # echo "[RTM] 2. Compiling FAST ==> MaxJ"
+    # find designs/ -name "*.c" -exec fastc -I../../include {} \; -exec bash -c   'dir=`dirname  "{}"` && mv build/engine/*.java "$dir"' \;
+    # echo "[RTM] 3. Save and Clean-up"
+    # mv designs designs-RTM
+    # rm -fr ADPKernel.c RTMKernel.c build rose_*
+
+
+    cp src/ADPKernel.c .
+    # --- ADP Code Generation
+    echo "[APD] 1. Running Harmonic weaver"
+    #larai $ASP_REP/main.lara -a "{csources:'ADPKernel.c', cflags: '-I../../include/', cparams:[ { N: 8 }, { N: 9}], mants:[10, 11]}"
+    larai $ASP_REP/harmonic.lara -x bin/
+    cp harmonic.xml $ASP_REP
+    larai $ASP_REP/main.lara -a "{csources:'ADPKernel.c', cflags: '-I../../include/', cparams:[ { N: 8 }], mants:[10], aspRoot: '$ASP_REP/'}"
+    # echo "[ADP] 2. Compiling FAST ==> MaxJ"
+    find designs/ -name "*.c" -exec maxcc -I../../include {} \; -exec bash -c  'dir=`dirname  "{}"` && mv build/engine/*.java "$dir"' \;
+    echo "[ADP] 3. Save and Clean-up"
+    mv designs designs-ADP
+    rm -rf ADPKernel.c RTMKernel.c build rose_*
+}
+
 setupFlag=$1
 
 case $setupFlag in
     -s) setup;;
     -p) package;;
     -c) clean;;
+    -r) run;;
     *)  help;;
 esac
