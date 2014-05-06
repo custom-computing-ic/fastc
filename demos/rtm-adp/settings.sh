b0897d8e8af5b45498bf48f305bf9724f304fbce
Paul Grigoras <paul.grigoras90@gmail.com>
2014-05-06 17:27:54 +0100

    Update binary path.
    Former-commit-id: 6da3c3f8baf67671c37b472c295320e1bcfbb3ce

--- demos/rtm-adp/settings.sh
+++ demos/rtm-adp/settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #binaries (harmonic,larai, fastc)
-export PATH=$(readlink -m bin/):${PATH}
+export PATH=$(readlink -m ../bin/):${PATH}
 
 #aspect repository
 export ASP_REP=$(readlink -m ../../aspects/)
